1f972a3f43b6820bf73c4554735a14bc744219fe
Malkan <malkanshaheen45@gmail.com>
2025-09-06 16:26:57 +0500

    backend added

--- api/send-email.js
+++ api/send-email.js
@@ -30,6 +30,7 @@ export default async function handler(req, res) {
 
     res.status(200).json({ message: "Email sent successfully!" });
   } catch (error) {
+     console.error("Email send error:", error);
     res.status(500).json({ message: "Failed to send email", error });
   }
 }
